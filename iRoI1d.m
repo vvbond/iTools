e8f4adcca9144b60c742f9b19aa3219655c9da4f
Vladimir Bondarenko <vbondarenko@>
2022-01-06 00:10:35 +0000

    iRoI1d: variious major improvements.

--- iRoI1d.m
+++ iRoI1d.m
@@ -38,8 +38,9 @@ classdef iRoI1d < iTool
 % (c) Vladimir Bondarenko, http://www.mathworks.co.uk/matlabcentral/fileexchange/authors/52876
     
     %% Properties
-    properties
-        facealpha = .1
+    properties(SetObservable)
+        alpha = .1
+        color = 'b';
 %         lineStyle           % string, specifies the color and style of the vertical lines.
 %         lineColor           % color of the two side lines.
 %         lineWidth           % scalar, defines the width of the vertical lines.
@@ -64,198 +65,230 @@ classdef iRoI1d < iTool
         
     methods
         %% Constructor
-        function r = iRoI1d(interval)
+        function roi = iRoI1d(interval)
             
             % Defaults:
-%             r.lineStyle = '-';
-%             r.lineColor = [.5 .5 .5];
-%             r.lineWidth = 2;
-%             r.displayPosition = [.65 .85 .1 .1];
-            r.flags.roiPlotOn = false;
+%             roi.lineStyle = '-';
+%             roi.lineColor = [.5 .5 .5];
+%             roi.lineWidth = 2;
+%             roi.displayPosition = [.65 .85 .1 .1];
+            roi.tag = 'roi';
+            roi.flags.roiPlotOn = false;
             if nargin == 0
                 interval = [];
             end
             
             % Display function:
-            r.displayFcn = @(roi) num2str(roi.width, '%5.2f');
-            if r.is_valid_handle('hax') && isa(r.handles.hax.XAxis, 'matlab.graphics.axis.decorator.DatetimeRuler')
-                r.displayFcn = @(roi) datestr(roi.width, 'MM:SS');
+            roi.displayFcn = @(roi) num2str(roi.width, '%5.2f');
+            if roi.is_valid_handle('hax') && isa(roi.handles.hax.XAxis, 'matlab.graphics.axis.decorator.DatetimeRuler')
+                roi.displayFcn = @(roi) datestr(roi.width, 'MM:SS');
             end            
 
             % Range initialization:
             if ~isempty(interval)
-                r.init(interval);
-                r.init_plot();
+                roi.init(interval);
+                roi.init_plot();
             else
-                r.store_window_callbacks();
-                r.handles.ilines(1) = ixline(r.handles.hax, []);
-                addlistener(r.handles.ilines(1), 'lineCreated', @(src,evt) r.ginput_end(src,evt));
+                roi.store_window_callbacks();
+                roi.handles.ilines(1) = ixline(roi.handles.hax, []);
+                addlistener(roi.handles.ilines(1), 'lineCreated', @(src,evt) roi.ginput_end(src,evt));
             end
             
 
         end
         
         %% Destructor
-        function delete(r)
-            delete(r.handles.ilines);
-            if r.is_valid_handle('rect'), delete(r.handles.rect); end
-            if r.is_valid_handle('infotext'), delete(r.handles.infotext); end
+        function delete(roi)
+            delete(roi.handles.ilines);
+            if roi.is_valid_handle('rect'), delete(roi.handles.rect); end
+            if roi.is_valid_handle('infotext'), delete(roi.handles.infotext); end
         end
 
         %% Init
-        function init(r, interval)
-            r.handles.ilines(1) = ixline(r.handles.hax, interval(1));
-            r.handles.ilines(2) = ixline(r.handles.hax, interval(2));
+        function init(roi, interval)
+            roi.handles.ilines(1) = ixline(roi.handles.hax, interval(1));
+            roi.handles.ilines(2) = ixline(roi.handles.hax, interval(2));
         end
 
-        function init_plot(r)
-            hold(r.handles.hax, 'on');
-            r.handles.rect = fill(r.vertices(:,1), r.vertices(:,2), 'b', 'FaceAlpha', r.facealpha);
-            hold(r.handles.hax, 'off');
+        function create_context_menu(roi)
+            roi.handles.cm = uicontextmenu;
+            roi.handles.cmenu(1) = uimenu(roi.handles.cm, 'Label', 'Zoom', 'tag', 'zoom_xlim_in', 'checked', 'off', 'callback', @(src,evt) roi.zoominout(src,evt));
+            roi.handles.cmenu(end+1) = uimenu(roi.handles.cm, 'Label', 'View data', 'tag', 'switch_roi_plot', 'checked', 'off', 'callback', @(src,evt) roi.switch_roi_viewdata(src,evt));
+            roi.handles.cmenu(end+1) = uimenu(roi.handles.cm, 'Label', 'Peaks Finder', 'checked', 'off', 'callback', @(src,evt) roi.roi_to_peaks_finder());
+            roi.handles.cmenu(end+1) = uimenu(roi.handles.cm, 'Label', 'Export to workspace', 'checked', 'off', 'Separator', 'on', 'callback', @(src,evt) roi.export_to_workspace());
+            roi.handles.cmenu(end+1) = uimenu(roi.handles.cm, 'Label', 'Delete', 'tag', 'delete', 'checked', 'off', 'callback', @(src,evt) roi.delete());
+            set(roi.handles.rect, 'UIContextMenu', roi.handles.cm);
+        end
+        
+        function init_plot(roi)
+            hold(roi.handles.hax, 'on');
+            roi.handles.rect = fill(roi.vertices(:,1), roi.vertices(:,2), roi.color, 'FaceAlpha', roi.alpha);
+            hold(roi.handles.hax, 'off');
 
-            r.display_info();
+            roi.display_info();
             
             % Interactivity:
-            addlistener(r.handles.ilines, 'positionChanged', @(src,evt) r.update() );
-            set(r.handles.rect, 'ButtonDownFcn', @(src,evt) rect_bdcb(r,src,evt));
-            
+            set(roi.handles.rect, 'ButtonDownFcn', @(src,evt) roi.rect_bdcb(src,evt));
+            addlistener(roi.handles.ilines, 'positionChanged', @(src,evt) roi.update() );
+            addlistener(roi, 'color', 'PostSet', @(src,evt) roi.rect_postset_cb());
+            addlistener(roi, 'alpha', 'PostSet', @(src,evt) roi.rect_postset_cb());
+
             % Context menu:
-            r.handles.cm = uicontextmenu;
-            r.handles.cmenu(1) = uimenu(r.handles.cm, 'Label', 'View data', 'tag', 'switch_roi_plot', 'checked', 'off', 'callback', @(src,evt) r.switch_roi_plot());
-            r.handles.cmenu(end+1) = uimenu(r.handles.cm, 'Label', 'Peaks Finder', 'checked', 'off', 'callback', @(src,evt) r.roi_find_peaks());
-            r.handles.cmenu(end+1) = uimenu(r.handles.cm, 'Label', 'Delete', 'tag', 'delete', 'checked', 'off', 'callback', @(src,evt) r.delete());
-            set(r.handles.rect, 'UIContextMenu', r.handles.cm);
+            roi.create_context_menu();
 
             % Notification:
-            notify(r,'plotCreated');
+            notify(roi,'plotCreated');
         end
         
         %% Interactions 
-        function ginput_end(r, ~, ~)
-            r.handles.ilines(2) = ixline(r.handles.hax, r.handles.ilines(1).Value);
-            r.init_plot();
+        function ginput_end(roi, ~, ~)
+            roi.handles.ilines(2) = ixline(roi.handles.hax, roi.handles.ilines(1).Value);
+            roi.init_plot();
             click_evt = struct('Button', 1);
-            r.handles.ilines(2).bdcb([], click_evt);
-            r.handles.listeners = event.listener(r.handles.ilines(2), 'buttonUp', @(src,evt) r.roi_created_cb(src,evt));
+            roi.handles.ilines(2).bdcb([], click_evt);
+            roi.handles.listeners = event.listener(roi.handles.ilines(2), 'buttonUp', @(src,evt) roi.roi_created_cb(src,evt));
         end
 
-        function roi_created_cb(r, ~, ~)
-            r.restore_window_callbacks();
-            delete(r.handles.listeners(end));
-            r.handles.listeners(end) = [];
-            notify(r, 'roiCreated');
+        function roi_created_cb(roi, ~, ~)
+            roi.restore_window_callbacks();
+            delete(roi.handles.listeners(end));
+            roi.handles.listeners(end) = [];
+            notify(roi, 'roiCreated');
         end
 
         % button down callback
-        function rect_bdcb(r,~,evt)
+        function rect_bdcb(roi,~,evt)
             
             if evt.Button ~= 1, return; end
-            dx = r.interval_num(1) - r.currentPoint(1);
-            r.store_window_callbacks();
-            set(r.handles.hfig, 'windowButtonMotionFcn', @(src,evt) wbmcb(r,src,evt, dx),...
-                                'windowButtonUpFcn',     @(src,evt) wbucb(r,src,evt),...
+            dx = roi.interval_num(1) - roi.currentPoint(1);
+            roi.store_window_callbacks();
+            set(roi.handles.hfig, 'windowButtonMotionFcn', @(src,evt) wbmcb(roi,src,evt, dx),...
+                                'windowButtonUpFcn',     @(src,evt) wbucb(roi,src,evt),...
                                 'pointer', 'left');
         end
                 
         % window button motion callback
-        function wbmcb(r,~,~, dx)
-            roi_width = diff(r.interval_num);
-            r.handles.ilines(1).Value = r.currentPoint(1) + dx;
-            r.handles.ilines(2).Value = r.handles.ilines(1).Value + roi_width;
+        function wbmcb(roi,~,~, dx)
+            roi_width = diff(roi.interval_num);
+            roi.handles.ilines(1).Value = roi.currentPoint(1) + dx;
+            roi.handles.ilines(2).Value = roi.handles.ilines(1).Value + roi_width;
         end
         
         % window button up callback
-        function wbucb(r,~,~)
-            r.restore_window_callbacks();
-            notify(r, 'buttonUp');
+        function wbucb(roi,~,~)
+            roi.restore_window_callbacks();
+            notify(roi, 'buttonUp');
         end        
         
         %% ROI update
-        function update(r)
-            set(r.handles.rect, 'XData', r.vertices(:,1), 'YData', r.vertices(:,2));
-            set(r.handles.infotext, 'Position', r.vertices([2,6]), 'String', r.displayFcn(r));
-            if r.flags.roiPlotOn
-                r.plot_data();
+        function update(roi)
+            set(roi.handles.rect, 'XData', roi.vertices(:,1), 'YData', roi.vertices(:,2));
+            set(roi.handles.infotext, 'Position', roi.vertices([2,6]), 'String', roi.displayFcn(roi));
+            if roi.flags.roiPlotOn
+                roi.view_data();
             end
-            notify(r, 'roiChanged');
+            notify(roi, 'roiChanged');
         end
         
         %% Annotation
-        function display_info(r)
-            r.handles.infotext = text(r.vertices(2,1), r.vertices(2,2), r.displayFcn(r), 'FontSize', 16);
+        function display_info(roi)
+            roi.handles.infotext = text(roi.vertices(2,1), roi.vertices(2,2), roi.displayFcn(roi), 'FontSize', 16);
         end
     end
 
     %% RoI operations
     methods
         % Check which elements of v fall within roi's interval. 
-        function ind = within(r, v)
-            ind = within(ruler2num(v, r.handles.hax.XAxis), r.interval_num);
+        function ind = within(roi, v)
+            ind = within(ruler2num(v, roi.handles.hax.XAxis), roi.interval_num);
         end
 
-        function plot_data(r)
-            if ~r.is_valid_handle('roiplot')
+        function view_data(roi)
+            if ~roi.is_valid_handle('roiview_plot')
                 T = [ 1 0 0 0;
                       0 1 0 -.5;
                       0 0 1 0;
                       0 0 0 .5 ];
-                hfig = figure('OuterPosition', get(r.handles.hfig, 'OuterPosition')*T');
-                r.handles.roiplot = plot(r.data.x, r.data.y);
+                hfig = figure('OuterPosition', get(roi.handles.hfig, 'OuterPosition')*T');
+                roi.handles.roiview_plot = plot(roi.data.x, roi.data.y);
+                roi.handles.roiview_hax = roi.handles.roiview_plot.Parent;
+                title(roi.handles.roiview_hax, sprintf('ROI: %s', range(roi.interval)), roi.interval);
                 grid on, box on, axis tight;
                 iAxes.set_keyboard_shortcuts(hfig);
+                iAxes.set_interactions(roi.handles.roiview_hax);
+                roi.handles.roiview_rlz     = iRulerz('x');
+                roi.handles.roiview_roitool = iRoI1d_tool;
             else
-                set(r.handles.roiplot, 'XData', r.data.x, 'YData', r.data.y);
+                set(roi.handles.roiview_plot, 'XData', roi.data.x, 'YData', roi.data.y);
+                title(roi.handles.roiview_hax, sprintf('ROI: %s', range(roi.interval)), roi.interval);
             end
         end
     
-        function roi_find_peaks(r)
-            r.handles.peaksFinder = iPeaksFinder(r.data.y, r.data.x);
+        function roi_to_peaks_finder(roi)
+            roi.handles.peaksFinder = iPeaksFinder(roi.data.y, roi.data.x);
+            roi.handles.peaksFinder.handles.Parent = roi;
         end
     end
 
     %% Setters & Getters
     methods
-        function val = get.interval_num(r)
-            val = [r.handles.ilines.Value];
+        function val = get.interval_num(roi)
+            val = [roi.handles.ilines.Value];
         end
 
-        function val = get.interval(r)
-            val = num2ruler(r.interval_num, r.handles.hax.XAxis);
+        function val = get.interval(roi)
+            val = num2ruler(roi.interval_num, roi.handles.hax.XAxis);
         end
 
-        function val = get.vertices(r)
-            ylims = get(r.handles.hax, 'YLim');
-            val = [r.interval_num([1 2 2 1]); ylims([1 1 2 2])]';
+        function val = get.vertices(roi)
+            ylims = get(roi.handles.hax, 'YLim');
+            val = [roi.interval_num([1 2 2 1]); ylims([1 1 2 2])]';
         end
 
-        function val = get.width(r)
-            val = diff(num2ruler(r.interval_num, r.handles.hax.XAxis));
+        function val = get.width(roi)
+            val = diff(num2ruler(roi.interval_num, roi.handles.hax.XAxis));
         end
 
-        function val = get.width_num(r)
-            val = diff(r.interval_num);
+        function val = get.width_num(roi)
+            val = diff(roi.interval_num);
         end
 
-        function data = get.data(r)
-            l = arrayfilter(@(a) isa(a, 'matlab.graphics.chart.primitive.Line'), r.handles.hax.Children);
+        function data = get.data(roi)
+            l = arrayfilter(@(a) isa(a, 'matlab.graphics.chart.primitive.Line'), roi.handles.hax.Children);
             if isempty(l)
                 data = [];
                 return;
             end
-            ind  = r.within(l(1).XData);
+            ind  = roi.within(l(1).XData);
             data = struct('x', l.XData(ind), 'y', l.YData(ind));
         end
     
-        function switch_roi_plot(r)
-            menuitem = findobj(r.handles.cm, 'tag', 'switch_roi_plot');
-            menuitem.Checked = ifthel(menuitem.Checked == "on", "off", "on");
-            if menuitem.Checked == "on"
-                r.flags.roiPlotOn = true;
-                r.plot_data();
+    end
+    %% Callbacks
+    methods
+        function switch_roi_viewdata(roi, src, ~)
+            src.Checked = ifthel(src.Checked == "on", "off", "on");
+            if src.Checked == "on"
+                roi.flags.roiPlotOn = true;
+                roi.view_data();
             else
-                r.flags.roiPlotOn = false;
+                roi.flags.roiPlotOn = false;
             end
+        end
 
+        function zoominout(roi, src, ~)
+            src.Checked = ifthel(src.Checked == "on", "off", "on");
+            if src.Checked == "on"
+                roi.cache.xlims = roi.handles.hax.XAxis.Limits;
+                xlim(roi.handles.hax, interval_resize(roi.interval, 1.1));
+            else
+                xlim(roi.handles.hax, roi.cache.xlims);
+            end
+        end
+
+        function rect_postset_cb(roi, ~, ~)
+            roi.handles.rect.FaceColor = roi.color;
+            roi.handles.rect.FaceAlpha = roi.alpha;
         end
     end
 end
