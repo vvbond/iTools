30aa9ff2095874e89d428246ebb337613581e13e
Vladimir Bondarenko <vbondarenko@>
2022-01-06 00:13:16 +0000

    iPeaksFinder: added export_to_workspace option, plot header, done button, interactive tools.

--- iPeaksFinder.m
+++ iPeaksFinder.m
@@ -10,10 +10,12 @@ classdef iPeaksFinder < iTool
     properties
         height_factor = .6
         prominence_factor = .5;
+        distance_factor = .7;
     end
     methods
         function pkf = iPeaksFinder(data, x)
             narginchk(1,2);
+            pkf.tag = 'pkf';
             if nargin == 1
                 x = 1:length(data);
             end
@@ -31,7 +33,7 @@ classdef iPeaksFinder < iTool
             [pks, locs, wdth, prom] = findpeaks(pkf.data, pkf.x);
             pkf.minheight = pkf.height_factor * median(pks);
             pkf.minprominence = pkf.prominence_factor * median(prom);
-            pkf.mindistance = median(diff(locs));
+            pkf.mindistance = pkf.distance_factor * median(diff(locs));
         end
 
         function find_peaks(pkf)
@@ -53,10 +55,13 @@ classdef iPeaksFinder < iTool
         function create_main_menu(pkf)
             pkf.handles.menu = uimenu(pkf.handles.hfig, 'Text', 'Peaks Finder');
             pkf.handles.menu_params = uimenu(pkf.handles.menu, 'Text', 'Params', 'MenuSelectedFcn', @(src,evt) pkf.menu_params_cb());
+            pkf.handles.menu_freqs  = uimenu(pkf.handles.menu, 'Text', 'Estimate peaks frequency', 'MenuSelectedFcn', @(src,evt) pkf.menu_freqs_cb());
+            pkf.handles.menu_export = uimenu(pkf.handles.menu, 'Text', 'Export to workspace', 'Separator', 'on', 'MenuSelectedFcn', @(src,evt) pkf.export_to_workspace());
         end
 
         function init_plot(pkf)
             pkf.handles.hfig = figure;
+            pkf.handles.hfig.Position = scrctr(1000, 500);
             pkf.handles.hax_prom = subplot(1, 30, 1);
             pkf.handles.hax = subplot(1, 30, 3:30);
             % Setup prominence "gauge" axes:
@@ -74,6 +79,8 @@ classdef iPeaksFinder < iTool
             hold off
             box on, grid on;
 
+            % Header:
+            pkf.display_plot_header();
 
             % Interactivity:
             pkf.handles.peaksplot.ButtonDownFcn = @(src,evt) pkf.peak_bdcb(src,evt);
@@ -87,12 +94,37 @@ classdef iPeaksFinder < iTool
 
             % Menus:
             pkf.create_main_menu();
+
+            % Toolbar:
+            pkf.handles.roitool = iRoI1d_tool;
+            pkf.handles.rlz = iRulerz('x');
+
+            % Buttons:
+            pkf.handles.btn_done = uicontrol(pkf.handles.hfig, ...
+                                             'Style', 'pushbutton', ...
+                                             'Position', [0 0 50 30], ...
+                                             'String', 'Done', ...
+                                             'FontSize', 14, ...
+                                             'BackgroundColor', 'g', ...
+                                             'Callback', @(src,evt) pkf.done_cb());
         end
 
         function update(pkf, recompute)
             if nargin == 1, recompute = true; end
             if recompute, pkf.find_peaks(); end
             set(pkf.handles.peaksplot, 'XData', pkf.peaks.location, 'YData', pkf.peaks.value);
+            pkf.display_plot_header();
+        end
+
+        function display_plot_header(pkf)
+            title(pkf.handles.hax, sprintf('Min peak height: %5.1f || Min peak prominence: %5.1f || Min peak distance: %s', ...
+                                            pkf.minheight, pkf.minprominence, pkf.mindistance));
+            subtitle(pkf.handles.hax, ...
+                  sprintf('Peaks #: %d || Avg. distance: %s || Avg. prominence: %5.1f || Avg. width: %s', ...
+                          length(pkf.peaks.value), ...
+                          mean(diff(pkf.peaks.location)), ...
+                          mean(pkf.peaks.prominence), ...
+                          mean(pkf.peaks.width)));
         end
     end
 
@@ -109,7 +141,7 @@ classdef iPeaksFinder < iTool
 
         function menu_params_cb(pkf)
             prompt = {'Minimum peak height', 'Minimum peak prominence', 'Minimum peak distance [s]'};
-            dlgttl = 'Peak parameters';
+            dlgtitle = 'Peak parameters';
             dims = [1, 20];
             definput = {num2str(pkf.minheight, '%5.1f'), num2str(pkf.minprominence, '%5.1f')};
             if isa(pkf.mindistance, 'duration')
@@ -117,7 +149,7 @@ classdef iPeaksFinder < iTool
             elseif isnumeric(pkf.mindistance)
                 definput{end+1} = num2str(pkf.mindistance);
             end
-            params = inputdlg(prompt(1:length(definput)), dlgttl, dims, definput);
+            params = inputdlg(prompt(1:length(definput)), dlgtitle, dims, definput);
             if isempty(params), return; end
             pkf.minheight = str2double(params{1});
             pkf.minprominence = str2double(params{2});
@@ -130,6 +162,21 @@ classdef iPeaksFinder < iTool
                 end
             end
             pkf.update();
+        end
+
+        function menu_freqs_cb(pkf)
+            prompt = {'Resolution'};
+            dlgtitle = 'Peaks frequency';
+            dims = [1, 20];
+            definput = num2str(30);
+        end
+
+        function done_cb(pkf)
+            pkf.flags.done = true;
+            if pkf.is_valid_handle('Parent') && isa(pkf.handles.Parent, 'iRoI1d')
+                pkf.handles.Parent.color = 'g';
+            end
+            close(pkf.handles.hfig);
         end
     end
     events
